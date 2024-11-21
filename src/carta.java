cb3e72f2230a7e5b7dd31f93dc4795d68628caf9
ariismuf <aris.s338835@cesurformacion.com>
2024-11-21 11:39:25 +0100

    primera clase

---
+++ src/carta.java
@@ -0,0 +1,28 @@
+public class carta {
+    private String palo;
+    private String valor;
+    private int puntos;
+
+    public carta(String palo, String valor, int puntos) {
+        this.palo = palo;
+        this.valor = valor;
+        this.puntos = puntos;
+    }
+
+    public String getPalo() {
+        return palo;
+    }
+
+    public String getValor() {
+        return valor;
+    }
+
+    public int getPuntos() {
+        return puntos;
+    }
+
+    @Override
+    public String toString() {
+        return valor + de + palo;
+    }
+}
